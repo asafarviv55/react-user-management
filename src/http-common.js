c2d60931f05b4115bb564caa2af96d70f1b507e1
asafarviv55 <asaf.arviv@gmail.com>
2025-11-26 14:40:59 +0200

    Update API URL to connect to nodejs-user-management backend on port 3001

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api/users";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api/users";
 
 export default axios.create({
   baseURL: API_URL,
